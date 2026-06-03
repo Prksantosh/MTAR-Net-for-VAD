515c11d35a6dc5c8b7303de973d04be5245f4df0
Prksantosh <santosh.prk22@gmail.com>
2026-06-03 18:32:12 +0530

    Update autoencoder_skip.py

--- models/autoencoder_skip.py
+++ models/autoencoder_skip.py
@@ -15,9 +15,7 @@ from models.mem_GALSTM import MemE3DLSTM
 #from models.mem_2d_lstm import MemE3DLSTM
 #from models.mema_e3d_emu import MemE3DLSTM
 
-###############################################
-# Full Autoencoder Model with Skip Connections
-###############################################
+
 class RHCNetAutoencoder(nn.Module):
 
     def __init__(self, seq_len=3):
@@ -26,9 +24,7 @@ class RHCNetAutoencoder(nn.Module):
 
         self.seq_len = seq_len
 
-        ###############################################
-        # Initial Feature Extraction
-        ###############################################
+
 
         self.initial = nn.Conv2d(
             3,
@@ -37,48 +33,35 @@ class RHCNetAutoencoder(nn.Module):
             padding=1
         )
 
-        ###############################################
-        # Encoder
-        ###############################################
+ 
 
         self.enc1 = EncoderStage(24, 48)
         self.enc2 = EncoderStage(48, 96)
         self.enc3 = EncoderStage(96, 192)
         self.enc4 = EncoderStage(192, 384)
 
-        ###############################################
-        # Timestamp Transformation
-        ###############################################
 
         self.timestamp = TimestampTransform()
 
-        ###############################################
-        # Temporal Modeling
-        ###############################################
+
 
         self.e3d = MemE3DLSTM(384, 384)
 
-        ###############################################
-        # Decoder
-        ###############################################
+
 
         self.dec1 = DecoderStage(384, 192)
         self.dec2 = DecoderStage(192, 96)
         self.dec3 = DecoderStage(96, 48)
         self.dec4 = DecoderStage(48, 24)
 
-        ###############################################
-        # Skip Fusion Layers
-        ###############################################
+
 
         self.skip1 = nn.Conv2d(192 + 192, 192, 1)
         self.skip2 = nn.Conv2d(96 + 96, 96, 1)
         self.skip3 = nn.Conv2d(48 + 48, 48, 1)
         self.skip4 = nn.Conv2d(24 + 24, 24, 1)
 
-        ###############################################
-        # Final Reconstruction
-        ###############################################
+
 
         self.final = nn.Conv2d(
             24,
@@ -91,49 +74,33 @@ class RHCNetAutoencoder(nn.Module):
 
         B, T, C, H, W = x.shape
 
-        ###############################################
-        # Merge Batch and Time
-        ###############################################
+
 
         x = x.view(B * T, C, H, W)
 
-        ###############################################
-        # Initial Feature Extraction
-        ###############################################
+
 
         x0 = self.initial(x)
 
-        ###############################################
-        # Encoder
-        ###############################################
 
-        e1 = self.enc1(x0)   # [BT,48,128,128]
-        e2 = self.enc2(e1)   # [BT,96,64,64]
-        e3 = self.enc3(e2)   # [BT,192,32,32]
-        e4 = self.enc4(e3)   # [BT,384,16,16]
 
-        ###############################################
-        # Convert to Spatiotemporal Tensor
-        # (B,C,T,H,W)
-        ###############################################
+        e1 = self.enc1(x0)   
+        e2 = self.enc2(e1)  
+        e3 = self.enc3(e2)  
+        e4 = self.enc4(e3)  
+
+
 
         x = self.timestamp(e4, B, T)
 
-        ###############################################
-        # Temporal Modeling
-        ###############################################
+
 
         x = self.e3d(x)
 
-        ###############################################
-        # Take Last Time Step
-        ###############################################
 
-        x = x[:, :, -1]   # [B,384,16,16]
+        x = x[:, :, -1] 
 
-        ###############################################
-        # Last Encoder Features
-        ###############################################
+
 
         e3_last = (
             e3.view(
@@ -175,11 +142,9 @@ class RHCNetAutoencoder(nn.Module):
             )[:, -1]
         )
 
-        ###############################################
-        # Decoder Stage 1
-        ###############################################
 
-        x = self.dec1(x)              # [B,192,32,32]
+
+        x = self.dec1(x)              
 
         x = torch.cat(
             [x, e3_last],
@@ -188,11 +153,9 @@ class RHCNetAutoencoder(nn.Module):
 
         x = self.skip1(x)
 
-        ###############################################
-        # Decoder Stage 2
-        ###############################################
 
-        x = self.dec2(x)              # [B,96,64,64]
+
+        x = self.dec2(x)             
 
         x = torch.cat(
             [x, e2_last],
@@ -201,11 +164,9 @@ class RHCNetAutoencoder(nn.Module):
 
         x = self.skip2(x)
 
-        ###############################################
-        # Decoder Stage 3
-        ###############################################
 
-        x = self.dec3(x)              # [B,48,128,128]
+
+        x = self.dec3(x)             
 
         x = torch.cat(
             [x, e1_last],
@@ -214,11 +175,9 @@ class RHCNetAutoencoder(nn.Module):
 
         x = self.skip3(x)
 
-        ###############################################
-        # Decoder Stage 4
-        ###############################################
 
-        x = self.dec4(x)              # [B,24,256,256]
+
+        x = self.dec4(x)             
 
         x = torch.cat(
             [x, x0_last],
@@ -227,9 +186,7 @@ class RHCNetAutoencoder(nn.Module):
 
         x = self.skip4(x)
 
-        ###############################################
-        # Final Reconstruction
-        ###############################################
+
 
         x = self.final(x)
 
